3ba80930b1d8af1de78ba0cd0e5f4ea2e2a3bc50
Alon Bukai <alon@camilyo.com>
2021-07-31 11:26:27 +0300

    Old

--- Assets/Scripts/KnightBehavior.cs
+++ Assets/Scripts/KnightBehavior.cs
@@ -5,6 +5,9 @@ using UnityEngine.AI;
 
 public class KnightBehavior : MonoBehaviour
 {
+    public Transform[] points;
+    private int destPoint = 0;
+
     private Animator anim;
     private NavMeshAgent agent;
     public GameObject target;
@@ -14,12 +17,37 @@ public class KnightBehavior : MonoBehaviour
         anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
 
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.transform.position);
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+
+        //agent.SetDestination(target.transform.position);
         /*
         if(Input.GetKey("x"))
         {
